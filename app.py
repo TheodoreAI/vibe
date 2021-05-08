a50069f60db99ffc487479935997c9635905a78e
Mateo Estrada <mateoej12@gmail.com>
2021-05-08 14:44:05 -0700

    Added two more movies and an input bar.

--- app.py
+++ app.py
@@ -16,16 +16,20 @@ app = dash.Dash(__name__)
 
 app.title = "Virtual Interface Bank of Emotions"
 
+ALLOWED_TYPES = (
+    "text",
+)
 app.layout = html.Div([
     html.Div([
-        html.H1(children="Vibe",),
+        html.H1(children="Vibe", ),
         html.P(
             children="The application where the atmosphere of a movie as communicated to and felt by others can be measured. ",
         ),
         dcc.Dropdown(
             id='my_dropdown',
             options=[
-                {'value': sent, 'label': sent} for sent in ["Terminator 2: Judgement Day", "Iron Man", "Up"]],
+                {'value': sent, 'label': sent} for sent in
+                ["Terminator 2: Judgement Day", "Iron Man", "Up", "Interstellar", "Por mis pistolas", "Saw"]],
             value="Terminator 2: Judgement Day",
             multi=False,
             clearable=False,
@@ -33,11 +37,26 @@ app.layout = html.Div([
         ),
     ]),
 
+    # The following code for the text input for the search query came from Dash source docs: https://dash.plotly.com/dash-core-components/input
+
+    html.Div([
+                 dcc.Input(
+                     id="input_{}".format(_),
+                     type=_,
+                     placeholder="Input type {}".format(_),
+                     className="Input-header"
+                 )
+                 for _ in ALLOWED_TYPES
+             ] + [html.Div(id="out-all-types")]
+
+             ),
+
     html.Div([
         dcc.Graph(
             id="the_graph"
         )
     ]),
+
 ])
 
 
@@ -57,13 +76,14 @@ def update_graph(values):
     # If its the right movie selected in the drop down, then get the data for that specific movie
     values_from_movie = []
     if list(df.to_dict()['title'].values())[0] == values:
-        labels = ['neg', 'pos', 'neu']
+        labels = ['neg', 'neu', 'pos']
         single_movie_neg = list(df.to_dict()['neg'].values())
-        single_movie_pos = list(df.to_dict()['pos'].values())
         single_movie_neu = list(df.to_dict()['neu'].values())
-        values_from_movie.append(single_movie_pos[0])
-        values_from_movie.append(single_movie_neu[0])
+        single_movie_pos = list(df.to_dict()['pos'].values())
         values_from_movie.append(single_movie_neg[0])
+        values_from_movie.append(single_movie_neu[0])
+        values_from_movie.append(single_movie_pos[0])
+
         print(values_from_movie)
 
     # labels = ['', 'Hydrogen', 'Carbon_Dioxide', 'Nitrogen']
@@ -75,6 +95,19 @@ def update_graph(values):
     fig = go.Figure(data=[go.Pie(labels=labels, values=values_from_movie, hole=.3)])
 
     return fig
+
+
+# This callback is for the inputs: this callback comes from the source code for Dash Input: https://dash.plotly.com/dash-core-components/input
+
+@app.callback(
+    Output("out-all-types", "children"),
+    [Input("input_{}".format(_), "value") for _ in ALLOWED_TYPES],
+)
+def cb_render(*vals):
+    return " | ".join((str(val) for val in vals if val))
+
+
+
 
 
 if __name__ == '__main__':
